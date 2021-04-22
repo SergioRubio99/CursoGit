b5487283a05e5a85c5dc691b65b8f8a16f3f4475
Sergio <srubiochavarria@gmail.com>
2021-04-22 14:48:23 +0200

    2nd paragraph & css mod

---
+++ js.js
@@ -0,0 +1,10 @@
+(function() {
+    let smartphone = document.getElementById("smartphone");
+    let top_container = document.getElementById("top-container");
+    ("top-container");
+
+    smartphone.addEventListener('click', function() {
+        top_container.style.display = top_container.style.display == 'flex' ? 'none' : 'flex'
+
+    });
+})();
